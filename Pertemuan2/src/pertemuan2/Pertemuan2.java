35aae1447358ab361df0e5239e03c8d672e640c5
OOP C <arthawebid@gmail.com>
2025-03-15 14:48:51 +0800

    Pertemuan 2

---
+++ Pertemuan2/src/pertemuan2/Pertemuan2.java
@@ -0,0 +1,39 @@
+package pertemuan2;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Made Artha
+ * TGL: 15 Maret 2025
+ */
+public class Pertemuan2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String mhs="Yuanita Sari Dewi";
+        long nim=9999999999L;
+        String kelas="A";
+                
+        BufferedReader dtIN = new BufferedReader(new InputStreamReader(System.in));
+        
+        try{
+            System.out.print("Input Nama Mahasiswa: ");
+            mhs = dtIN.readLine();
+            
+            System.out.print("Input NIM: ");
+            nim = Long.parseLong( dtIN.readLine() );            
+            
+        }catch(IOException e){
+            System.out.println("Terjadi masalah saat baca keyboard");
+        }
+        
+        kelas = JOptionPane.showInputDialog("Input Kelas: ");
+        
+        System.out.println("Data "+mhs+" dengan NIM "+ nim+ " berada di kelas "+kelas);
+    }
+    
+}
